2fa0a554f343955694798a452dcba12fbfce714b
jfisher2021 <j.fisher.2021@alumnos.urjc.es>
2023-12-14 21:07:41 +0100

    Nuevo wifi en la ESP

---
+++ credentials.h
@@ -0,0 +1,16 @@
+// credentials.h
+
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// Replace with your actual values
+#define EAP_IDENTITY "j.fisher.2021@alumnos.urjc.es"
+#define EAP_PASSWORD ""
+#define EAP_USERNAME "j.fisher.2021@alumnos.urjc.es"
+
+#define MQTT_SERVER "garceta.tsc.urjc.es"
+#define MQTT_PORT 21883
+#define MQTT_USERNAME "Dios_te_ama"
+#define MQTT_KEY "33"
+
+#endif // CREDENTIALS_H
