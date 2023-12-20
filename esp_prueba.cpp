ddce115ee4364d59ba59ba288d82bb79d807fc00
Jonathan Fisher <j.fisher.2021@alumnos.urjc.es>
2023-12-20 16:45:55 +0100

    Se supone que va la distancia y el tiempo final

--- esp_prueba.cpp
+++ esp_prueba.cpp
@@ -2,11 +2,11 @@
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 
-const char* ssid = "LosPollosHermanos";
-const char* password = "tdko9519";
+const char *ssid = "fisherwifi";
+const char *password = "solomio333";
 
-const char* topic = "/SETR/2023/10/";
-const char* id_equipo = "10";
+const char *topic = "/SETR/2023/10/";
+const char *id_equipo = "10";
 
 unsigned long TimeStart = 0;
 unsigned long lastPingTime = 0;
@@ -23,16 +23,19 @@ uint16_t MQTT_PORT = 21883;
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_PORT, MQTT_USERNAME, MQTT_KEY);
 
-
 unsigned long start_time = millis();
+unsigned long final_time;
+int dintance = 0;
 String dist = "";
+String final_time_str = "";
 
-
-void connectToWiFi() {
+void connectToWiFi()
+{
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid,password);
+  WiFi.begin(ssid, password);
   Serial.print("Wifi connecting...");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print('.');
     delay(1000);
   }
@@ -41,11 +44,13 @@ void connectToWiFi() {
   Serial.println(WiFi.localIP());
 }
 
-void connectToMQTT() {
+void connectToMQTT()
+{
   Serial.println("Connecting to MQTT...");
 
   int8_t ret;
-  while ((ret = mqtt.connect()) != 0) {
+  while ((ret = mqtt.connect()) != 0)
+  {
     Serial.println(mqtt.connectErrorString(ret));
     Serial.println("Retrying MQTT connection in 5 seconds...");
     mqtt.disconnect();
@@ -55,108 +60,124 @@ void connectToMQTT() {
   Serial.println("MQTT Connected!");
 }
 
-void publishData(const char* data) {
-  if (mqtt.publish(topic, data)) {
+void publishData(const char *data)
+{
+  if (mqtt.publish(topic, data))
+  {
     Serial.println("Data published to MQTT");
-  } else {
+  }
+  else
+  {
     Serial.println("Failed to publish data");
   }
 }
 
-void start_lap_message() {
+void start_lap_message()
+{
   char message[256];
   sprintf(message, "{\n\t\"team_name\": \"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"START_LAP\"\n}", id_equipo);
   publishData(message);
 }
 
-void end_lap_message(unsigned long time) {
+void end_lap_message(unsigned long time)
+{
   char message[256];
-  sprintf(message, "{\n\t\"team_name\":\"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"END_LAP\",\n\t\"time\": %ld\n}", id_equipo,time);
+  sprintf(message, "{\n\t\"team_name\":\"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"END_LAP\",\n\t\"time\": %ld\n}", id_equipo, time);
   publishData(message);
 }
 
-void track_loose_message() {
+void track_loose_message()
+{
   char message[256];
   sprintf(message, "{\n\t\"team_name\":\"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"LINE_LOST\"\n}", id_equipo);
   publishData(message);
 }
 
-void obstacle_detection_message() {
+void obstacle_detection_message()
+{
   char message[256];
-  sprintf(message, "{\n\t\"team_name\":\"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"OBSTACLE_DETECTED\",\n\t\"distance\": %s\n}", id_equipo,dist);
-  //sprintf(message, "{\n\t\"team_name\":\"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"OBSTACLE_DETECTED\"\n}", id_equipo);
+  sprintf(message, "{\n\t\"team_name\":\"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"OBSTACLE_DETECTED\",\n\t\"distance\": %d\n}", id_equipo, dintance);
+  // sprintf(message, "{\n\t\"team_name\":\"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"OBSTACLE_DETECTED\"\n}", id_equipo);
   publishData(message);
 }
 
-void ping_message(unsigned long time) {
+void ping_message(unsigned long time)
+{
   char message[256];
-  sprintf(message, "{\n\t\"team_name\":\"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"PING\",\n\t\"time\": %ld\n}", id_equipo,time);
+  sprintf(message, "{\n\t\"team_name\":\"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"PING\",\n\t\"time\": %ld\n}", id_equipo, time);
   publishData(message);
 }
 
-void searching_line_message() {
+void search()
+{
   char message[256];
   sprintf(message, "{\n\t\"team_name\":\"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"SEARCHING_LINE\"\n}", id_equipo);
   publishData(message);
 }
 
-void line_found() {
-  char message[256];
-  sprintf(message, "{\n\t\"team_name\":\"DIOS_TE_AMA\",\n\t\"id\":\"%s\",\n\t\"action\":\"LINE_FOUND\"\n}", id_equipo);
-  publishData(message);
-}
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  //Ardiuino
-  Serial2.begin(9600,SERIAL_8N1, RXD2, TXD2);
+  // Ardiuino
+  Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
 }
 
 String receive_buff;
 
-void loop() {
-  //able to communicate with arduino
-  if (Serial2.available()) {
+void loop()
+{
+  // able to communicate with arduino
+  if (Serial2.available())
+  {
 
-    //read from arduino
+    // read from arduino
     char c = Serial2.read();
     receive_buff += c;
 
-    if (c == '}'){
+    if (c == '}')
+    {
 
-      //connect to wifi
+      // connect to wifi
       connectToWiFi();
-      //connect to mqtt
-      if (!mqtt.connected()) {
+      // connect to mqtt
+      if (!mqtt.connected())
+      {
         connectToMQTT();
       }
-      if (receive_buff == "{CONNECT}"){
+      if (receive_buff == "{CONNECT}")
+      {
         connectToMQTT();
-      } else if(receive_buff == "{START_LAP}"){
-        start_time = millis();
-        //time for pinging
-        partial_time= millis();
+      }
+      else if (receive_buff == "{SP}")
+      {
+        // time for pinging
+        partial_time = millis();
         start_lap_message();
-
-      } else if (receive_buff == "{LINE_LOST}"){
-        track_loose_message();
-        searching_line_message();
-
-      } else if (receive_buff == "{OBSTACLE_DETECTED}"){
-
-        dist = Serial2.readStringUntil('{');
-        obstacle_detection_message();
-      } else if(receive_buff == "{PING}"){  
-        ping_message(millis()-start_time);
-
-      } else if(receive_buff == "{END_LAP}"){
-        end_lap_message(millis()-start_time);
-      } else if (receive_buff == "{LINE_FOUND}") {
-        line_found();
+        start_time = millis();
       }
+      else if (receive_buff == "{LT}")
+      {
+        track_loose_message();
+      }
+      else if (receive_buff == "{OD}")
+      {
+        dist = Serial2.readStringUntil('{');
+        dintance = dist.toInt();
 
-      receive_buff="";
+        obstacle_detection_message();
+      }
+      else if (receive_buff == "{PG}")
+      {
+        ping_message(millis() - start_time);
+      }
+      else if (receive_buff == "{EL}")
+      {
+        final_time_str = Serial2.readStringUntil('{');
+        final_time = final_time_str.toInt();
 
+        end_lap_message(final_time);
+      }
+      receive_buff = "";
     }
   }
 }
